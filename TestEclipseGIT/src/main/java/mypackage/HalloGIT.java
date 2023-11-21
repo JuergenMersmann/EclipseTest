7ba9da799f1faadae27ac669c39d86ff40b6fe1b
Juergen Mersmann <juergen@mersmann.org>
2023-11-21 20:15:02 +0100

    Init

---
+++ TestEclipseGIT/src/main/java/mypackage/HalloGIT.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class HalloGIT {
+
+	public HalloGIT() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
